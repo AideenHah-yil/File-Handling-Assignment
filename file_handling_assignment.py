c542efe1271aa7160d4c880c198afe5a3401771c
Ruth Wangare AideenHah-yil <wangarenjorogeruth@gmail.com>
2024-03-23 14:10:32 +0300

    working on tasks remaining

--- file_handling_assignment.py
+++ file_handling_assignment.py
@@ -4,3 +4,10 @@ with open("my_file.txt", 'w') as f:
         f.write("Third line\n")
          
        
+with open("my_file.txt", 'r') as f:
+        f.read()
+
+with open("my_file.txt", 'a+') as f:    
+        f.write("First line\n")
+        f.write("fifth line\n")
+        f.write("sixth line\n")    
