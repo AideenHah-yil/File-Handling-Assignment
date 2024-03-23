757e00d16943130bee4c75cbd18dfcde6aed1417
Ruth Wangare AideenHah-yil <wangarenjorogeruth@gmail.com>
2024-03-23 14:13:55 +0300

    3 tasks down

--- file_handling_assignment.py
+++ file_handling_assignment.py
@@ -5,9 +5,10 @@ with open("my_file.txt", 'w') as f:
          
        
 with open("my_file.txt", 'r') as f:
-        f.read()
+        file_content = f.read()
+        print(file_content)
 
-with open("my_file.txt", 'a+') as f:    
-        f.write("First line\n")
+with open("my_file.txt", 'a') as f:    
+        f.write("Fourth line\n")
         f.write("fifth line\n")
         f.write("sixth line\n")    
